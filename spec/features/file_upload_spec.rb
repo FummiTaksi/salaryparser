f10154eb83be08dbce0d833dc80c7fbd3a73391f
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-11-19 20:12:22 +0200

    added more tests for file-upload

--- spec/features/file_upload_spec.rb
+++ spec/features/file_upload_spec.rb
@@ -19,6 +19,38 @@ describe "Uploading file from the root-page" do
       click_on("Import CSV");
       expect(page).to have_content "File not .csv type!"
     end
+
+    it "user imports a file with unvalid date" do
+      attach_file("file", Rails.root + "spec/testfiles/dateIsNotValid.csv");
+      click_on("Import CSV");
+      expect(page).to have_content "File is in wrong format. Please modify file and try again."
+    end
+
+    it "user imports a file with unvalid end time" do
+      attach_file("file", Rails.root + "spec/testfiles/endTimeNotCorrect.csv");
+      click_on("Import CSV");
+      expect(page).to have_content "File is in wrong format. Please modify file and try again."
+    end
+
+    it "user imports a file where name column is empty" do
+      attach_file("file", Rails.root + "spec/testfiles/nameIsEmpty.csv");
+      click_on("Import CSV");
+      expect(page).to have_content "File is in wrong format. Please modify file and try again."
+    end
+
+    it "user imports a file where personId contains letters" do
+      attach_file("file", Rails.root + "spec/testfiles/personIdIsAString.csv");
+      click_on("Import CSV");
+      expect(page).to have_content "File is in wrong format. Please modify file and try again."
+    end
+
+    it "user imports a file where start time is not valid" do
+      attach_file("file", Rails.root + "spec/testfiles/startTimeNotCorrect.csv");
+      click_on("Import CSV");
+      expect(page).to have_content "File is in wrong format. Please modify file and try again."
+    end
+
+
   end
 
   describe "is successful when " do
