34ee84ad2f2c883cfc21663fddb1282fa1a6a803
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-11-19 11:38:53 +0200

    added capybara tests for file uploading

---
+++ spec/features/file_upload_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+
+describe "Uploading file from the root-page" do
+
+  before :each do
+    visit root_path
+  end
+
+  describe "fails when" do
+
+    it "user clicks import button without selecting a file " do
+      click_on("Import CSV")
+      expect(page).to have_content "File not found!"
+    end
+
+    it "user imports .txt file " do
+      attach_file("file", Rails.root + "spec/testfiles/test.txt");
+      click_on("Import CSV");
+      expect(page).to have_content "File not .csv type!"
+    end
+  end
+
+  describe "is successful when " do
+
+    it "user imports a .csv file " do
+      attach_file("file", Rails.root + "spec/testfiles/correctWageFile.csv");
+      click_on("Import CSV");
+      expect(page).to have_content "File uploaded successfully!"
+    end
+
+  end
+end
