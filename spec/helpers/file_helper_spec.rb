7c5bbe53305216b13fe410864ad074383449e2fa
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-11-19 11:39:54 +0200

    created more tests for refactored code

--- spec/helpers/file_helper_spec.rb
+++ spec/helpers/file_helper_spec.rb
@@ -1,23 +1,78 @@
 require 'rails_helper'
 
 RSpec.describe FileHelper, type: :helper do
-
-  it "returns true if filename is .csv" do
-    csvFile = getFile("wage.csv")
-    expect(isFileCsv(csvFile)).to eq true
-  end
-
-  it "returns false if filename is .pdf" do
-    pdfFile = getFile("wage.pdf")
-    expect(isFileCsv(pdfFile)).to eq false
-  end
-
-  it "returns false if there is no file" do
-    expect(isFileCsv(nil)).to eq false
-  end
+  let(:csvFile){ getCsvFile()}
+  let(:pdfFile){getPdfFile()}
 
   def getFile(fileName)
     return ActionDispatch::Http::UploadedFile.new tempfile: 'tempfile', filename: fileName
   end
+
+  def getCsvFile()
+    return getFile("file.csv");
+  end
+
+  def getPdfFile()
+    return getFile("file.pdf");
+  end
+
+  describe "isFileCsv" do
+
+    it "returns true if filename is .csv" do
+      expect(isFileCsv(csvFile)).to be_truthy
+    end
+
+    it "returns false if filename is .pdf" do
+      expect(isFileCsv(pdfFile)).to be_falsey
+    end
+  end
+
+  describe "isFileDefined" do
+
+    it "returns true when file is defined" do
+      expect(isFileDefined(csvFile)).to be_truthy
+    end
+
+    it "returns false if file is nil " do
+      expect(isFileDefined(nil)).to be_falsey
+    end
+
+  end
+
+  describe "defineFileType" do
+
+    it "shows flash notice if file is .csv type" do
+        defineFileType(csvFile);
+        expect(flash[:notice]).to be_present
+    end
+
+    it "shows flash error if file is not .csv type" do
+        defineFileType(pdfFile);
+        expect(flash[:error]).to be_present
+    end
+
+  end
+
+  describe "handleFile" do
+
+    it "shows flash error if file is not defined" do
+        handleFile(nil);
+        expect(flash[:error]).to be_present
+    end
+
+    it "shows flash notice if file is .csv type" do
+        handleFile(csvFile);
+        expect(flash[:notice]).to be_present
+    end
+
+    it "shows flash error if file is not .csv type" do
+        handleFile(pdfFile);
+        expect(flash[:error]).to be_present
+    end
+  end
+
+
+
+
 
 end
