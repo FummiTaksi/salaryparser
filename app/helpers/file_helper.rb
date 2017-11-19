016950b8a91decf70b1dccb39a5049d9a228b707
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-11-19 11:41:01 +0200

    created all the methods for file control

--- app/helpers/file_helper.rb
+++ app/helpers/file_helper.rb
@@ -1,11 +1,28 @@
 module FileHelper
 
   def isFileCsv(file)
-    if file.nil?
-      return false;
-    end
     fileName = file.original_filename;
     return File.extname(fileName) == ".csv";
+  end
+
+  def handleFile(file)
+    if (isFileDefined(file))
+      defineFileType(file)
+    else
+      flash[:error] = "File not found!"
+    end
+  end
+
+  def defineFileType(file)
+    if (isFileCsv(file))
+      flash[:notice]= "File uploaded successfully!"
+    else
+      flash[:error] = "File not .csv type!"
+    end
+  end
+
+  def isFileDefined(file)
+    return !file.nil?
   end
 
 end
