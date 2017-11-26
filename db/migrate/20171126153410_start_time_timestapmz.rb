1b3c0f0b7179145990c23da8976f2a95b2a2d03b
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-11-26 17:36:43 +0200

    trying with timestampz

---
+++ db/migrate/20171126153410_start_time_timestapmz.rb
@@ -0,0 +1,6 @@
+class StartTimeTimestapmz < ActiveRecord::Migration
+  def change
+    change_column :workshifts, :starttime, :timestampz
+    change_column :workshifts, :starttime, :timestampz
+  end
+end
