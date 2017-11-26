4b917d44c77ca512aa5e23c2f3274b6567a992e6
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-11-26 19:06:32 +0200

    calculation for evening hours ready

--- app/models/workshift.rb
+++ app/models/workshift.rb
@@ -11,4 +11,40 @@ class Workshift
     @starttime = attributes[:starttime]
     @endtime = attributes[:endtime]
   end
+
+  def countTheNormalWage
+    return countDifferenceInHours * 3.75;
+  end
+
+  def countEveningBonusTime
+    sixInTheMorning = DateTime.new(starttime.year, starttime.month, starttime.day + 1 , 6, 0, 0);
+    if shiftStartedInNightHours
+      return shiftEndsInNightHours ? countDifferenceInHours : (sixInTheMorning - starttime) * 24.0;
+    else
+       return countEveningBonusTimeWhenItStartsBeforeEveningHours;
+    end
+  end
+
+  def calculateEveningBonus
+    return countEveningBonusTime * 1.15;
+  end
+
+  def countEveningBonusTimeWhenItStartsBeforeEveningHours
+    sixInTheEvening = DateTime.new(starttime.year, starttime.month, starttime.day, 18, 0, 0);
+    if shiftEndsInNightHours
+      return (endtime -  sixInTheEvening) * 24.0;
+    else
+      return endtime.day > starttime.day ? 10.0 : 0;
+    end
+  end
+
+  def shiftStartedInNightHours
+    return starttime.hour >= 18 || starttime.hour < 6
+  end
+
+  def shiftEndsInNightHours
+    return endtime.hour >= 18 || endtime.hour < 6
+  end
+
+
 end
--- spec/models/workshift_spec.rb
+++ spec/models/workshift_spec.rb
@@ -11,4 +11,143 @@ RSpec.describe Workshift, type: :model do
     end
   end
 
+  describe "countNormalWage " do
+     it "counts it correctly " do
+       startTime = DateTime.new(2015,5,5,21,30,0);
+       endTime = DateTime.new(2015,5,5,23,0,0)
+       workshift = Workshift.new(starttime: startTime, endtime: endTime);
+       expect(workshift.countTheNormalWage).to eq 5.625
+     end
+  end
+
+  describe "shiftStartedInNightHours " do
+
+    describe "returns true when " do
+
+      it "start time is at 18" do
+        startTime = DateTime.new(2015,5,5,18,0,0);
+        endTime = DateTime.new(2015,5,5,23,0,0)
+        workshift = Workshift.new(starttime: startTime, endtime: endTime);
+        expect(workshift.shiftStartedInNightHours).to be_truthy
+      end
+
+      it "start time is at 5" do
+        startTime = DateTime.new(2015,5,5,5,59,0);
+        endTime = DateTime.new(2015,5,5,23,0,0)
+        workshift = Workshift.new(starttime: startTime, endtime: endTime);
+        expect(workshift.shiftStartedInNightHours).to be_truthy
+      end
+    end
+
+    describe "returns false when " do
+      it "starts in 6 " do
+        startTime = DateTime.new(2015,5,5,6,0,0);
+        endTime = DateTime.new(2015,5,5,23,0,0)
+        workshift = Workshift.new(starttime: startTime, endtime: endTime);
+        expect(workshift.shiftStartedInNightHours).to be_falsey
+      end
+    end
+  end
+
+  describe "shiftEndsInNightHours " do
+    describe "returns true when " do
+
+      it "end time is at 18" do
+        startTime = DateTime.new(2015,5,5,20,0,0);
+        endTime = DateTime.new(2015,5,5,18,0,0)
+        workshift = Workshift.new(starttime: startTime, endtime: endTime);
+        expect(workshift.shiftEndsInNightHours).to be_truthy
+      end
+
+      it "end time is at 5" do
+        startTime = DateTime.new(2015,5,5,23,59,0);
+        endTime = DateTime.new(2015,5,5,5,0,0)
+        workshift = Workshift.new(starttime: startTime, endtime: endTime);
+        expect(workshift.shiftEndsInNightHours).to be_truthy
+      end
+    end
+
+    describe "returns false when " do
+      it "ends in 6 " do
+        startTime = DateTime.new(2015,5,5,23,0,0);
+        endTime = DateTime.new(2015,5,5,6,0,0)
+        workshift = Workshift.new(starttime: startTime, endtime: endTime);
+        expect(workshift.shiftEndsInNightHours).to be_falsey
+      end
+    end
+  end
+
+  describe "countEveningBonusTime " do
+
+    describe "when shift starts in evening hours " do
+
+      describe "and ends in evening hours " do
+
+        it "method returns correct " do
+          startTime = DateTime.new(2015,5,5,23,0,0);
+          endTime = DateTime.new(2015,5,6,3,0,0)
+          workshift = Workshift.new(starttime: startTime, endtime: endTime);
+          expect(workshift.countEveningBonusTime).to eq 4.0
+        end
+
+      end
+
+      describe "and ends after evening hours " do
+
+          it "method returns correct " do
+            startTime = DateTime.new(2015,5,5,23,0,0);
+            endTime = DateTime.new(2015,5,6,7,0,0)
+            workshift = Workshift.new(starttime: startTime, endtime: endTime);
+            expect(workshift.countEveningBonusTime).to eq 7.0
+          end
+
+      end
+    end
+
+    describe "when shift starts before evening hours " do
+
+      describe "and ends before evening hours " do
+
+        it "method returns 0" do
+          startTime = DateTime.new(2015,5,5,16,0,0);
+          endTime = DateTime.new(2015,5,5,17,59,0)
+          workshift = Workshift.new(starttime: startTime, endtime: endTime);
+          expect(workshift.countEveningBonusTime).to eq 0.0
+        end
+
+      end
+
+      describe "and ends in evening hours " do
+
+        it "method returns correct" do
+          startTime = DateTime.new(2015,5,5,16,0,0);
+          endTime = DateTime.new(2015,5,5,23,0,0)
+          workshift = Workshift.new(starttime: startTime, endtime: endTime);
+          expect(workshift.countEveningBonusTime).to eq 5.0
+        end
+
+      end
+
+      describe "and ends after evening hours " do
+
+        it "method returns 10 " do
+          startTime = DateTime.new(2015,5,5,16,0,0);
+          endTime = DateTime.new(2015,5,6,6,1,0)
+          workshift = Workshift.new(starttime: startTime, endtime: endTime);
+          expect(workshift.countEveningBonusTime).to eq 10.0
+        end
+
+      end
+    end
+  end
+
+  describe "calculateEveningBonus " do
+    it "calculates correct number " do
+      startTime = DateTime.new(2015,5,5,16,0,0);
+      endTime = DateTime.new(2015,5,6,6,1,0)
+      workshift = Workshift.new(starttime: startTime, endtime: endTime);
+      expect(workshift.calculateEveningBonus).to eq 11.5
+    end
+  end
+
 end
